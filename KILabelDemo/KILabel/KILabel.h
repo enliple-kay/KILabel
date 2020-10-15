abde0cf911b61192300e906f99ef6e84f01f166b
kay <ickang@enliple.com>
2020-10-15 15:57:24 +0900

    add framework

---
+++ KILabelDemo/KILabel/KILabel.h
@@ -0,0 +1,19 @@
+//
+//  KILabel.h
+//  KILabel
+//
+//  Created by kay on 2020/10/15.
+//  Copyright © 2020 Matthew Styles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KILabel.
+FOUNDATION_EXPORT double KILabelVersionNumber;
+
+//! Project version string for KILabel.
+FOUNDATION_EXPORT const unsigned char KILabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KILabel/PublicHeader.h>
+
+
